d32b4c785f28c9d3bfa8c9f429107663d48a0103
leo <leo7412369@gmail.com>
2024-09-09 20:54:50 +0800

    帳號功能 - 1. MySecurityConfig.java - 增加securityFilterChain方法 2. authorizeRequests - API權限白名單，黑名單設定(無須登入即可訪問, 需要登入才能訪問, 都不能訪問)

--- src/main/java/com/example/springboot_security/controller/MemberController.java
+++ src/main/java/com/example/springboot_security/controller/MemberController.java
@@ -1,10 +1,36 @@
 package com.example.springboot_security.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springboot_security.dao.MemberDao;
+import com.example.springboot_security.model.Member;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class MemberController {
+
+    @Autowired
+    private MemberDao memberDao;
+
+    // 註冊使用者帳號
+    @PostMapping("/register")
+    public Member register(@RequestBody Member member) {
+
+        Integer memberId = memberDao.createMember(member);
+
+        return memberDao.getMemberById(memberId);
+    }
+
+    @GetMapping("/welcom")
+    public String welcome() {
+        return "welcom";
+    }
+    
 
     @GetMapping("/hello")
     public String hello() {
--- src/main/java/com/example/springboot_security/security/MySecurityConfig.java
+++ src/main/java/com/example/springboot_security/security/MySecurityConfig.java
@@ -2,18 +2,23 @@ package com.example.springboot_security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.Customizer;
 
 
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
 
+    // 設定密碼加密方式
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
@@ -39,4 +44,22 @@ public class MySecurityConfig {
     //     // 將用戶存儲在內存中
     //     return new InMemoryUserDetailsManager(userTest1, userTest2);
     // }
+
+    // 設定安全過濾器鏈
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        
+        // 設定不使用 CSRF，啟用 HTTP Basic 認證，啟用表單登入
+        return http
+            .csrf(csrf -> csrf.disable())
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults())
+
+            .authorizeRequests(request -> request
+                .requestMatchers("/register", "/welcom").permitAll()
+                .requestMatchers(HttpMethod.GET, "/hello/**").authenticated()
+                .anyRequest().denyAll()
+            )
+            .build();
+    }
 }
